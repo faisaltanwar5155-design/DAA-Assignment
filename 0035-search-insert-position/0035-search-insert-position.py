c5743e7cc6d1223ec4792960f77af85fb6216531
faisaltanwar5155-design <faisaltanwar5155@gmail.com>
2026-08-04 22:49:09 +0530

    Time: 3 ms (4.45%), Space: 12.9 MB (65.88%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+                break
+            elif nums[i] > target:
+                return i
+                break
+
+        if target > nums[-1]:
+            return len(nums)
+
+        
